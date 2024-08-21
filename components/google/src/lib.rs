5059c21e96b282c11e88c1a991caf2b64ef26209
Jakub Dulny <jdulny@connve.com>
2024-08-21 09:44:05 +0200

    Added references to MPL-2.0 licence

--- components/google/src/lib.rs
+++ components/google/src/lib.rs
@@ -1,8 +1,11 @@
+/// This Source Code Form is subject to the terms of the Mozilla Public
+/// License, v. 2.0. If a copy of the MPL was not distributed with this
+/// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #[path = ""]
 pub mod storage {
     #[path = "google.storage.v2.rs"]
     pub mod v2;
-    pub const ENDPOINT: &'static str = "https://storage.googleapis.com";
+    pub const ENDPOINT: &str = "https://storage.googleapis.com";
 }
 
 #[path = ""]
