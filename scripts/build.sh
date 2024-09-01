77af0f5dccd3e1776c7c289d63cc0f773e5da9ae
Jakub Dulny <jdulny@connve.com>
2024-09-01 21:08:16 +0200

    Fix for failing pipeline

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ apt update -qq
 apt-get -qq install pkg-config libssl-dev protobuf-compiler
 apt-get install git
 rustup component add rustfmt
-rm components/google/proto && git clone https://github.com/googleapis/googleapis components/google/proto
-rm components/salesforce/proto && git clone https://github.com/forcedotcom/pub-sub-api components/salesforce/proto
+rm -r components/google/proto && git clone https://github.com/googleapis/googleapis components/google/proto
+rm -r components/salesforce/proto && git clone https://github.com/forcedotcom/pub-sub-api components/salesforce/proto
 cargo build --verbose
 cargo test --verbose
 cargo fmt --all
