b8830cf8284496f7e0c613e958e711d9ab80548b
Jakub Dulny <jdulny@connve.com>
2024-09-01 21:18:55 +0200

    Fix for failed flowgen_salesforce build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ apt update -qq
 apt-get -qq install pkg-config libssl-dev protobuf-compiler
 apt-get install git
 rustup component add rustfmt
-rm -r components/google/proto && git clone https://github.com/googleapis/googleapis components/google/proto
+rm -r components/google/proto && git clone https://github.com/googleapis/googleapis components/google/proto/googleapis
 rm -r components/salesforce/proto && git clone https://github.com/forcedotcom/pub-sub-api components/salesforce/proto/pubsub
 cargo build --verbose
 cargo test --verbose
