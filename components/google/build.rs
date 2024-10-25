f3ff0d22c9a75df4dc0c7258fed9a7ac481e4d1c
Jakub Dulny <jdulny@connve.com>
2024-10-25 08:57:08 +0200

    Added type_attributes wide setup

--- components/google/build.rs
+++ components/google/build.rs
@@ -8,6 +8,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(true)
         .build_server(false)
         .out_dir("src")
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile_well_known_types(true)
         .compile(
             &["proto/googleapis/google/storage/v2/storage.proto"],
             &["proto/googleapis"],
--- components/salesforce/src/lib.rs
+++ components/salesforce/src/lib.rs
@@ -1,10 +1,10 @@
 /// This Source Code Form is subject to the terms of the Mozilla Public
 /// License, v. 2.0. If a copy of the MPL was not distributed with this
 /// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-#[path = ""]
 pub mod eventbus {
-    #[path = "eventbus.v1.rs"]
-    pub mod v1;
+    pub mod v1 {
+        include!("eventbus.v1.rs");
+    }
     pub const ENDPOINT: &str = "https://api.pubsub.salesforce.com";
     pub const DE_ENDPOINT: &str = "https://api.deu.pubsub.salesforce.com";
 }
