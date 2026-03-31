248d0902d83cffbcacbd52b7a58e578f4a606fa2
Jakub Dulny <142199348+jdulny@users.noreply.github.com>
2026-03-31 02:34:54 +0200

    feat: structured logging (#127)

--- flowgen-worker/core/src/task/generate/subscriber.rs
+++ flowgen-worker/core/src/task/generate/subscriber.rs
@@ -273,7 +273,7 @@ impl crate::task::runner::Runner for Subscriber {
         })
     }
 
-    #[tracing::instrument(skip(self), name = "task.run", fields(flow = %self.task_context.flow.name, task = %self.config.name, task_id = self.task_id, task_type = %self.task_type))]
+    #[tracing::instrument(skip(self), name = "task.run", fields(task = %self.config.name, task_id = self.task_id, task_type = %self.task_type))]
     async fn run(self) -> Result<(), Error> {
         let retry_config =
             crate::retry::RetryConfig::merge(&self.task_context.retry, &self.config.retry);
--- flowgen-worker/http/src/request.rs
+++ flowgen-worker/http/src/request.rs
@@ -279,7 +279,7 @@ impl flowgen_core::task::runner::Runner for Processor {
         Ok(event_handler)
     }
 
-    #[tracing::instrument(skip(self), name = "task.run", fields(flow = %self.task_context.flow.name, task = %self.config.name, task_id = self.task_id, task_type = %self.task_type))]
+    #[tracing::instrument(skip(self), name = "task.run", fields(task = %self.config.name, task_id = self.task_id, task_type = %self.task_type))]
     async fn run(mut self) -> Result<(), Error> {
         let retry_config =
             flowgen_core::retry::RetryConfig::merge(&self.task_context.retry, &self.config.retry);
